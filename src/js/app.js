160b371c78ac51987d2bbf8e0713199f75827b0d
ferdous <hassanferdous101@gmail.com>
2022-09-09 12:59:13 +0600

    added let-visit graphic and marquee

--- src/js/app.js
+++ src/js/app.js
@@ -3,40 +3,45 @@ import Splide from '@splidejs/splide';
 
 window.addEventListener('DOMContentLoaded', () => {
 	//init course marquee;
-	let courseMarquee = document.querySelector('.course__marquee');
-	if (courseMarquee) {
-		new marquee(courseMarquee, {
-			duplicated: true,
-			speed: 100,
-			recalcResize: true,
-			startVisible: true,
+	let marquees = [...document.querySelectorAll('.marquee')];
+	let courseCarousel = document.querySelector('#upcomingCourseSlider');
+	if (marquees.length) {
+		marquees.forEach((item) => {
+			new marquee(item, {
+				duplicated: true,
+				speed: 100,
+				recalcResize: true,
+				startVisible: true,
+			});
 		});
 	}
 
-	let upcomingCourseSlider = new Splide('#upcomingCourseSlider', {
-		perPage: 4,
-		type: 'loop',
-		gap: 43,
-		pagination: false,
-		breakpoints: {
-			1600: {
-				perPage: 3,
-				gap: 20,
-			},
-			992: {
-				perPage: 2,
-				gap: 16,
-				padding: {
-					right: 80,
+	if (courseCarousel) {
+		let upcomingCourseSlider = new Splide('#upcomingCourseSlider', {
+			perPage: 4,
+			type: 'loop',
+			gap: 43,
+			pagination: false,
+			breakpoints: {
+				1600: {
+					perPage: 3,
+					gap: 20,
+				},
+				992: {
+					perPage: 2,
+					gap: 16,
+					padding: {
+						right: 80,
+					},
+				},
+				660: {
+					perPage: 1,
+					padding: {
+						right: 30,
+					},
 				},
 			},
-			660: {
-				perPage: 1,
-				padding: {
-					right: 30,
-				},
-			},
-		},
-	});
-	upcomingCourseSlider.mount();
+		});
+		upcomingCourseSlider.mount();
+	}
 });
